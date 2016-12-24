25dec20262f579bade42dc449ad7e418eef16b2e
Prashanth Reddy Billa <Billa@Prashanths-MacBook-Air.local>
2016-12-24 11:27:32 -0500

    enabling cors support

--- src/main/java/com/mockii/Application.java
+++ src/main/java/com/mockii/Application.java
@@ -9,10 +9,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.mockii.entity")
 public class Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
--- src/main/java/com/mockii/controller/TopicController.java
+++ src/main/java/com/mockii/controller/TopicController.java
@@ -6,15 +6,12 @@ package com.mockii.controller;
 import com.mockii.dao.TopicDAO;
 import com.mockii.entity.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 
-@RestController
+@RestController(value = "/topic")
 public class TopicController {
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
